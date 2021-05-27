477e772c3cee2bbfd9ce3113c1449149235c7b09
Daniel Cajiao <cajiao.dani@gmail.com>
2021-05-26 20:36:56 -0400

    Adds initial Grabber functionality

--- Source/Building_Escape/Grabber.h
+++ Source/Building_Escape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
 #include "Grabber.generated.h"
 
 
@@ -24,5 +26,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	float Reach = 100.f;
 		
 };
